eedba418bfe7db9eca0fe1754269555936382196
lbranch <lbranch@yahoo.com>
2014-08-22 21:13:34 -0400

    sub functions

--- cachematrix.R
+++ cachematrix.R
@@ -11,6 +11,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 ## Function Description
+##
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+## Supporting
