c229660cab9d3faab732df40eee77fc3bb298466
lbranch <lbranch@yahoo.com>
2014-08-24 01:12:04 -0400

    Signed-off-by: lbranch <lbranch@yahoo.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,3 @@
- 
 ## Collaborator: Lonnell Branch
 ## Assignment 2
 ## R Programming Class 
@@ -6,45 +5,51 @@
 ###  makeCacheMatrix function creates a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
-
-## set the values of the matrix
-
-set<-function(y){
-  		x<<-y
-  		m<<-NULL
-	}
-
-## get the values of the matrix
-
-get<-function() x
-
-## Set the values of the inverse of the matix
-
-setInverse<-function(solve) m<<- solve
-
-## get the values of the inverse matrix
-
-getInverse<-function() m
-
-list(set=set, get=get,
-   setInverse=setInverse,
-   getInverse=getInverse)
-}
+    m<-NULL
+    
+    ## set the values of the matrix
+    
+    set<-function(y){
+        x<<-y
+        m<<-NULL
+    }
+    
+    ## get the values of the matrix
+    
+    get<-function() x
+    
+    ## Set the values of the inverse of the matix
+    
+    setInverse<-function(solve) m<<- solve
+    
+    ## get the values of the inverse matrix
+    
+    getInverse<-function() m
+    
+    list(set=set, get=get,
+         setInverse=setInverse,
+         getInverse=getInverse)
+} ## end makeCacheMatrix 
 
 
 ## Function to cache the inverse of the matrix
 
 cacheSolve <- function(x=matrix(), ...) {
+
     m<-x$getInverse()
-    if(!is.null(m)){
-      message("getting cached data")
-      return(m)
+
+## Check to see if inversed matrix loaded
+
+    if(!is.null(m)){   
+        message("getting cached data")
+        return(m)
     }
+## If inverse of matrix not loaded then inverse the matrix
+
     matrix<-x$get()
     m<-solve(matrix, ...)
     x$setInverse(m)
     m
-}
+}  ## end cacheSolve 
 
 ## Supporting
